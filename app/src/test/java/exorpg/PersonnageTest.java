a2170403acceb385d5a7576b6d2cc8513560f645
Florin Strulea <coolfloboy@gmail.com>
2022-07-18 11:42:16 +0200

    J'ai ajouté des tests pour personnage

---
+++ app/src/test/java/exorpg/PersonnageTest.java
@@ -0,0 +1,43 @@
+package exorpg;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import exorpg.RPG.Arme;
+import exorpg.RPG.Armure;
+import exorpg.RPG.Personnage;
+
+public class PersonnageTest {
+    protected static Arme poings = new Arme("Poings", 10, 0.01f);
+    protected static Armure aucune = new Armure("Aucune", 0);
+
+    protected Armure armor = aucune;
+    protected Arme equipedWeapon = poings;
+
+    @Test
+    public void getHit() {
+
+        Personnage perso = new Personnage("John", 100, 30);
+        perso.setArmor(armor);
+        perso.setEquipedWeapon(equipedWeapon);
+
+        perso.prendreCoup(10);
+
+        assertTrue(perso.getPv() == 90);
+    }
+
+    @Test
+    public void attack() {
+        Personnage perso = new Personnage("John", 100, 30);
+        Personnage villain = new Personnage("Tom", 100, 30);
+        perso.setArmor(armor);
+        perso.setEquipedWeapon(equipedWeapon);
+        villain.setArmor(armor);
+        villain.setEquipedWeapon(equipedWeapon);
+
+        perso.attaquer(villain);
+        assertTrue(villain.getPv() == 60);
+    }
+
+}
